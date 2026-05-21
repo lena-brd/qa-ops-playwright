f7916a11e257e510b8ddf5d17a3d894c24cb808e
lena-brd <elenabrd11@gmail.com>
2026-05-21 21:23:01 +0300

    updated checkout page

--- pageobjects/CheckoutPage.js
+++ pageobjects/CheckoutPage.js
@@ -14,7 +14,8 @@ class CheckoutPage {
   }
 
   async submitOrder(countryNameText) {
-    await this.checkoutButton.waitFor({ state: 'visible', timeout: 60000 });
+    await this.page.waitForLoadState('networkidle');
+    await this.page.waitForTimeout(3000);
     await this.checkoutButton.click();
     await this.notValidValueCard.clear();
     await this.placeholderCountry.pressSequentially(countryNameText);
